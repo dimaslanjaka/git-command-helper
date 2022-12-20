95146e5d5bf4fed2ab1901d47407ee80fb2210c1
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-12-20 15:01:13 +0700

    Create git-info.ts

---
+++ src/git-info.ts
@@ -0,0 +1,7 @@
+// get root directory of local repository
+// https://stackoverflow.com/a/957978
+// spawnAsync('git', 'rev-parse --show-toplevel'.split(' '))
+
+// get origin url
+// https://stackoverflow.com/a/4090938
+// spawnAsync('git', 'config --get remote.origin.url'.split(' '))
