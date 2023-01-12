4f6fc84fe42a219067b70efb15ba92f7878e95c2
Dimas Lanjaka <dimaslanjaka@gmail.com>
2023-01-12 22:19:56 +0700

    Update git-filter-branch.ts

--- src/git-filter-branch.ts
+++ src/git-filter-branch.ts
@@ -1,4 +1,12 @@
 /*
+bfg: https://rtyley.github.io/bfg-repo-cleaner/
+
+# clone mirror
+git clone --mirror git://example.com/some-big-repo.git
+
+# bfg clean files bigger than 100M
+java -jar bfg.jar --strip-blobs-bigger-than 100M some-big-repo.git
+
 # remove path from commit history
 git filter-branch --force --index-filter "git rm -rf --cached --ignore-unmatch <relative_path_to_file>" --prune-empty --tag-name-filter cat -- --all
 
