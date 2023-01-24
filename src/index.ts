2bcc120d1917985da0c99093a8b5a539e8bedc2a
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-24 16:46:42 +0700

    chore: fix and separate exports

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,5 @@
-import git, { gitCommandHelper, gitHelper, setupGit } from './git';
-import {
-  default as GithubInfo,
-  getGithubBranches,
-  getGithubCurrentBranch,
-  getGithubRemote,
-  getGithubRepoUrl,
-  getGithubRootDir
-} from './git-info';
-import { spawn, spawnAsync, spawnSilent } from './spawn';
-import { default as gitSubmodule } from './submodule';
-export { SpawnOptions } from './spawn';
-
-export const ext = {
-  spawn,
-  spawnAsync,
-  spawnSilent,
-  gitCommandHelper,
-  gitHelper,
-  setupGit,
-  GithubInfo,
-  getGithubBranches,
-  getGithubCurrentBranch,
-  getGithubRemote,
-  getGithubRepoUrl,
-  getGithubRootDir,
-  gitSubmodule
-};
+import git from './git';
+export * from './index-exports';
+export * as ext from './index-exports';
 
 export default git;
