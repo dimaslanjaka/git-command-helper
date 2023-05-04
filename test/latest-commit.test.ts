a74fe022c023d533d59a1fb940364f941beb5188
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-05 02:11:08 +0700

    refactor(latestCommit): update test

--- test/latest-commit.test.ts
+++ test/latest-commit.test.ts
@@ -11,7 +11,14 @@ describe('latestCommit() - get latest commit', () => {
 
   it('root repository', async () => {
     const commit = await gh.latestCommit();
-    console.log({ commit });
+    expect(typeof commit === 'string').toBeTruthy();
     expect(commit).toBe('ca0d4d0');
+  });
+
+  it('README.md', async () => {
+    const commit = await gh.latestCommit('README.md');
+    expect(typeof commit === 'string').toBeTruthy();
+    if (commit) expect('9e6355ad21e9d555418c4092cb60b5a67242c676'.includes(commit)).toBeTruthy();
+    expect(await gh.latestCommit('README.md', { short: false })).toBe('9e6355ad21e9d555418c4092cb60b5a67242c676');
   });
 });
