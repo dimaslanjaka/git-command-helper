0d4cef59c3cfa798f46d63d2e1596b8b6acafbed
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-09 01:50:21 +0700

    refactor: update test

--- test/check-ignores.test.ts
+++ test/check-ignores.test.ts
@@ -1,16 +1,23 @@
 import { beforeAll, describe, expect, it } from '@jest/globals';
 import { writeFileSync } from 'fs';
 import { join } from 'path';
-import { getIgnores } from '../src/functions/gitignore';
+import { getIgnores, isIgnored } from '../src/functions/gitignore';
 import { testcfg } from './config';
 
 describe('.gitignore test', () => {
+  const ignoredFile = join(testcfg.cwd, 'file-ignore.txt');
+
   beforeAll(() => {
-    writeFileSync(join(testcfg.cwd, 'file-ignore.txt'), '');
+    writeFileSync(ignoredFile, '');
   });
 
   it('should have file-ignore.txt', async () => {
     const check = await getIgnores(testcfg);
     expect(check.includes('file-ignore.txt')).toBeTruthy();
+  });
+
+  it('should be ignored', async () => {
+    const check = await isIgnored(ignoredFile);
+    expect(check).toBeTruthy();
   });
 });
