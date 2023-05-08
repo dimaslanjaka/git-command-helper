ed5510d666deae3dfabbea80e5d02de3d56d6661
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-09 01:34:36 +0700

    feat: add `verbose` property

--- src/functions/infoOptions.ts
+++ src/functions/infoOptions.ts
@@ -5,4 +5,8 @@ export interface infoOptions extends spawnAsync.SpawnOptions {
    * make function throws when error occurs
    */
   throwable?: boolean;
+  /**
+   * verbose
+   */
+  verbose?: boolean;
 }
