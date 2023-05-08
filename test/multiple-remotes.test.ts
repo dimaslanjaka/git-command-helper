b0ecf3efab96cd77d1e14d67a33e1684699c0faa
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-08 20:47:43 +0700

    refactor(test): update `multiple-remotes`

--- test/multiple-remotes.test.ts
+++ test/multiple-remotes.test.ts
@@ -1,17 +1,25 @@
-import { beforeAll, describe, expect, it } from '@jest/globals';
-import { gitHelper } from '../src';
+import { afterAll, beforeAll, describe, expect, it } from '@jest/globals';
+import { gitHelper, spawn } from '../src';
 import { testcfg } from './config';
 
 describe('test multiple url', () => {
   const github = new gitHelper(testcfg.cwd);
 
   beforeAll(async () => {
-    // set new remote
+    // set new remote upstream
     await github.setremote('https://github.com/dimaslanjaka/hexo', 'upstream');
   });
 
+  afterAll(() => {
+    // remove upstream
+    spawn('git', ['remote', 'remove', 'upstream'], { cwd: testcfg.cwd });
+  });
+
   it('should valid origin', async () => {
-    // github.getGithubRemote().then(console.log);
     expect((await github.getremote())?.push.url).toBe(testcfg.remote);
+  });
+
+  it('should valid upstream', async () => {
+    expect(await github.getremote('upstream')).toBe('https://github.com/dimaslanjaka/hexo');
   });
 });
