22358f4eaae5f3856a66e14701804b7576af3f9b
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-08 18:19:05 +0700

    feat: add multiple remotes test

---
+++ test/multiple-remotes.test.ts
@@ -0,0 +1,17 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+import { gitHelper } from '../src';
+import { testcfg } from './config';
+
+describe('test multiple url', () => {
+  const github = new gitHelper(testcfg.cwd);
+
+  beforeAll(async () => {
+    // set new remote
+    await github.setremote('https://github.com/dimaslanjaka/hexo', 'upstream');
+  });
+
+  it('should valid origin', async () => {
+    // github.getGithubRemote().then(console.log);
+    expect(await github.getremote()).toBe(testcfg.remote);
+  });
+});
