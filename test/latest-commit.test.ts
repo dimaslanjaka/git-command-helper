c7b4e7444f5c1d8aebf6105346af4d3f3baa1514
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-09 00:27:55 +0700

    refactor: update test

--- test/latest-commit.test.ts
+++ test/latest-commit.test.ts
@@ -10,14 +10,14 @@ describe('latestCommit() - get latest commit', () => {
   });
 
   it('root repository', async () => {
-    expect(await gh.latestCommit(undefined, { short: true })).toBe('ca0d4d0');
-    expect(await gh.latestCommit(undefined, { short: false })).toBe('ca0d4d0343dfe8f2b1000e4085f5c2fc10e0f3d2');
-    expect(await gh.latestCommit()).toBe('ca0d4d0');
+    expect(await gh.latestCommit(undefined, { short: true })).toBe('def54d6c');
+    expect(await gh.latestCommit(undefined, { short: false })).toBe('def54d6c2839626137ed73f93075ab3325d11408');
+    expect(await gh.latestCommit()).toBe('def54d6c');
   });
 
   it('README.md', async () => {
     const shortHash = await gh.latestCommit('README.md', { short: true });
-    expect(shortHash).toBe('9e6355a');
+    expect(shortHash).toBe('9e6355ad');
     const longHash = await gh.latestCommit('README.md', { short: false });
     expect(longHash).toBe('9e6355ad21e9d555418c4092cb60b5a67242c676');
   });
