730530de4c8bc095a94a1482ca2e5065366c0de1
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-09 01:16:53 +0700

    fix: true case path

--- src/functions/getGithubRootDir.ts
+++ src/functions/getGithubRootDir.ts
@@ -1,15 +1,19 @@
+import path from 'upath';
 import noop from '../noop';
 import { spawnAsync } from '../spawn';
+import { trueCasePathSync } from '../utils/case-path';
+import { infoOptions } from './infoOptions';
 
 /**
  * get root directory of local repository
  * * see {@link https://stackoverflow.com/a/957978}
  * @returns
  */
-export async function getGithubRootDir(opt: spawnAsync.SpawnOptions = {}) {
+export async function getGithubRootDir(opt: infoOptions = {}) {
+  if (!opt.cwd) opt.cwd = process.cwd();
   try {
-    const result = await spawnAsync('git', 'rev-parse --show-toplevel'.split(' '), opt);
-    return result.stdout.trim();
+    const result = await spawnAsync('git', ['rev-parse', '--show-toplevel'], opt);
+    return path.toUnix(trueCasePathSync(result.stdout.trim()));
   } catch (err) {
     return noop(err);
   }
