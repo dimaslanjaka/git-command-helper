f2d57b81c659c58df8e0e3ebf13bb1b88333934d
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-09 01:52:10 +0700

    feat: add ignore checker

--- src/functions/gitignore.ts
+++ src/functions/gitignore.ts
@@ -1,4 +1,10 @@
 import cp from 'cross-spawn';
+import fs from 'fs';
+import { minimatch } from 'minimatch';
+import path from 'upath';
+import { trueCasePathSync } from '../utils/case-path';
+import { getGithubRootDir } from './getGithubRootDir';
+import { infoOptions } from './infoOptions';
 
 /**
  * get all ignored files by .gitignore
@@ -11,3 +17,46 @@ export const getIgnores = async ({ cwd = process.cwd() }) =>
     .map((str) => str.trim())
     .filter((str) => str.startsWith('!!'))
     .map((str) => str.replace(/!!\s+/, ''));
+
+export type Return = {
+  filter: {
+    str: string;
+    cwd: string;
+    relativePath: string;
+    matched: boolean;
+  }[];
+  result: boolean;
+};
+
+export async function isIgnored(filePath: string): Promise<boolean>;
+export async function isIgnored(filePath: string, opt?: infoOptions): Promise<boolean | Return>;
+export async function isIgnored(filePath: string, opt: { verbose: true }): Promise<Return>;
+export async function isIgnored(filePath: string, opt: { verbose: false }): Promise<boolean>;
+export async function isIgnored(filePath: string, { verbose = false }: infoOptions = {}) {
+  const cwd = await getGithubRootDir({ cwd: path.dirname(filePath) });
+  if (!cwd) throw new Error(filePath + ' is not inside git repository');
+
+  const ignores = await getIgnores({ cwd });
+  const filter = ignores.map((str) => {
+    const unixPath = path.toUnix(trueCasePathSync(filePath));
+    let relativePath = unixPath;
+    if (fs.existsSync(unixPath)) {
+      relativePath = unixPath.replace(cwd, '');
+    }
+    relativePath = relativePath.replace(/^\/+/, '');
+    return {
+      str,
+      cwd,
+      relativePath,
+      matched: str === relativePath || relativePath.startsWith(str) || minimatch(relativePath, str)
+    };
+  });
+  const result = filter.some((o) => o.matched);
+  if (verbose) {
+    return {
+      filter,
+      result
+    };
+  }
+  return result;
+}
