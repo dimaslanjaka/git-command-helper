0a8ca1a3b2e377d6b99bdf029995d8b36b547dc4
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-09 16:54:52 +0700

    docs: update JSDoc

--- src/utils/safe-url.ts
+++ src/utils/safe-url.ts
@@ -2,10 +2,8 @@
  * remove `token` from url.
  *
  * @example
- * ```log
- * from : https://TOKEN@github.com/username/repository-name/blob/filename#L01-L202
- * to   : https://github.com/username/repository-name/blob/filename#L01-L202
- * ```
+ * // from : https://TOKEN@github.com/username/repository-name/blob/filename#L01-L202
+ * // to   : https://github.com/username/repository-name/blob/filename#L01-L202
  * @param url
  * @returns
  */
