fde37a80708f29bcb23e32d78520447162fdbad3
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-27 14:36:46 +0700

    refactor: update `index.ts` builder

--- src/functions/index.builder.ts
+++ src/functions/index.builder.ts
@@ -1,5 +1,19 @@
 import fs from 'fs-extra';
+import path from 'path';
+import { spawnSilent } from '../spawn';
 
-fs.readdirSync(__dirname).forEach((file) => {
-  console.log(file);
+// src/functions/index.ts exports builder
+// this only for development and excluded from build config
+
+// create export
+const contents = fs.readdirSync(__dirname).map((file) => {
+  return `export * from './${file.replace(/.ts$/, '')}';`;
 });
+// dump
+console.log(contents);
+// fix eslint
+contents.push('', '//', '');
+
+fs.writeFileSync(path.join(__dirname, 'index.ts'), contents.join('\n'));
+
+spawnSilent('eslint', ['--fix', 'src/**/*.ts'], { cwd: path.join(__dirname, '../..') });
