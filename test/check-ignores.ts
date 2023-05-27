4128dec221d51847cb5e93fc07e59a6b8f9312ac
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-27 16:59:34 +0700

    refactor: using `clone()` instead import

--- test/check-ignores.ts
+++ test/check-ignores.ts
@@ -1,10 +1,11 @@
 import fs from 'fs';
 import path from 'upath';
 import { isIgnored } from '../src/functions/gitignore';
+import clone from './clone';
 import { testcfg } from './config';
 
 (async function () {
-  await import('./pretest');
+  await clone();
   const ignoredFile = path.join(testcfg.cwd, 'file-ignore.txt');
   const ignoredFile2 = path.join(testcfg.cwd, 'file-ignore-another.txt');
   fs.writeFileSync(ignoredFile, '');
