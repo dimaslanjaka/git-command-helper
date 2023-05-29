2553b1f3b101681569aad8a5d1efd2bc8f2b3d8e
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-29 11:51:20 +0700

    chore: update submodule interface

--- src/utils/extract-submodule.ts
+++ src/utils/extract-submodule.ts
@@ -4,8 +4,11 @@ import { dirname, join } from 'upath';
 import git, { GitOpt } from '../git';
 
 export interface Submodule extends GitOpt {
-  github?: git;
-  path?: string;
+  github: git;
+  path: string;
+  url: string;
+  branch: string;
+  cwd: string;
 }
 
 /**
@@ -18,7 +21,8 @@ function extractSubmodule(gitmodulesPath: string) {
     if (key.startsWith('submodule')) {
       const submodule = config[key] as Submodule;
       submodule.cwd = join(dirname(String(gitmodulesPath)), submodule.path);
-      submodule.github = new git(submodule.cwd);
+      const github = new git(submodule.cwd);
+      submodule.github = github;
       return submodule;
     }
   });
