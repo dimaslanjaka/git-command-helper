5d7d228d4b936690a9b43f5a528410e970ce4603
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-30 03:43:58 +0700

    chore: update import

--- src/spawn.ts
+++ src/spawn.ts
@@ -2,7 +2,7 @@
 
 import Bluebird from 'bluebird';
 import { CommonSpawnOptions } from 'child_process';
-import * as sysSpawn from '../cross-spawn/src';
+import * as sysSpawn from '../cross-spawn/src/index';
 export { default as spawnAsync } from '@expo/spawn-async';
 export * from '../cross-spawn/src';
 // import { spawn as sysSpawn } from 'child_process';
