ab1c50245b3c1f6149c136cd999f959a0eddacc2
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-30 01:44:23 +0700

    chore: clean build

--- dist/index-exports.d.ts
+++
@@ -1,9 +0,0 @@
-export { getGithubBranches } from './functions/getGithubBranches';
-export { getGithubCurrentBranch } from './functions/getGithubCurrentBranch';
-export { getGithubRemote } from './functions/getGithubRemote';
-export { getGithubRepoUrl } from './functions/getGithubRepoUrl';
-export { getGithubRootDir } from './functions/getGithubRootDir';
-export * from './git';
-export { default as GithubInfo } from './git-info';
-export * from './spawn';
-export { default as gitSubmodule } from './submodule';
