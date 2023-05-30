1dc175955bae9fb5fce6c4d69dfb8d229e82f8d0
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-30 09:53:42 +0700

    refactor: validate empty folder fix: relative clone path

--- test/clone.ts
+++ test/clone.ts
@@ -1,20 +1,33 @@
 import 'core-js/actual';
-import { existsSync } from 'fs';
-import { join } from 'path';
-import { spawn } from '../src';
+import fs from 'fs-extra';
+import path from 'upath';
+import git from '../src';
+
+const spawn = git.crossSpawn.spawnAsync;
 
 /**
  * do clone when destination folder not exist
  */
-async function clone(TestConfig: { cwd: string; remote: string; branch: string; token?: string }) {
+async function clone(options: { cwd: string; remote: string; branch: string; token?: string }) {
   // if (existsSync(TestConfig.cwd)) await rm(TestConfig.cwd, { recursive: true, force: true });
-  const cfg = structuredClone(TestConfig);
+  const cfg = structuredClone(options);
   delete cfg.token;
 
-  if (!existsSync(TestConfig.cwd)) {
+  let doClone = false;
+  if (!fs.existsSync(options.cwd)) {
+    doClone = true;
+  } else if (fs.existsSync(options.cwd) && fs.readdirSync(options.cwd).length === 1) {
+    doClone = true;
+    await fs.rm(options.cwd, { recursive: true, force: true });
+  }
+
+  if (doClone) {
     console.log('cloning', cfg);
-    await spawn('git', ['clone', '-b', TestConfig.branch, TestConfig.remote, 'tmp/project-test'], {
-      cwd: join(__dirname, '../'),
+    const processCwd = path.join(__dirname, '..');
+    const relative = path.toUnix(options.cwd).replace(processCwd, '').replace(/^\/+/, '');
+    console.log({ processCwd, relative });
+    await spawn('git', ['clone', '-b', options.branch, options.remote, relative], {
+      cwd: processCwd,
       stdio: 'inherit'
     });
   }
