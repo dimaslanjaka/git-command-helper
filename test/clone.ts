64e1318d168cd74df6f05064165dbe26b4b93769
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-30 09:14:49 +0700

    refactor: add parameter option

--- test/clone.ts
+++ test/clone.ts
@@ -2,15 +2,14 @@ import 'core-js/actual';
 import { existsSync } from 'fs';
 import { join } from 'path';
 import { spawn } from '../src';
-import { TestConfig } from './config';
 
 /**
  * do clone when destination folder not exist
  */
-async function clone() {
+async function clone(TestConfig: { cwd: string; remote: string; branch: string; token?: string }) {
   // if (existsSync(TestConfig.cwd)) await rm(TestConfig.cwd, { recursive: true, force: true });
   const cfg = structuredClone(TestConfig);
-  delete cfg.password;
+  delete cfg.token;
 
   if (!existsSync(TestConfig.cwd)) {
     console.log('cloning', cfg);
