f9c97c6a848d0d5decebde6b608d2125c91b058c
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-30 10:50:03 +0700

    feat: add end of line handling

--- src/config/gitConfig.ts
+++ src/config/gitConfig.ts
@@ -21,4 +21,22 @@ export class gitConfig {
   ignoreCase(toBe: boolean | string) {
     return this.custom('core.ignorecase', String(toBe));
   }
+
+  /**
+   * set end of line all files
+   * @param type
+   * @returns
+   */
+  eol(type: 'lf' | 'crlf') {
+    return this.custom('core.eol', type);
+  }
+
+  /**
+   * The `git config core.autocrlf` command is used to change how Git handles line endings. It takes a single argument.
+   * @param toBe true - Configure Git to ensure line endings in files you checkout are correct for Windows.
+   * @returns
+   */
+  autocrlf(toBe: boolean | string) {
+    return this.custom('core.autocrlf', String(toBe));
+  }
 }
