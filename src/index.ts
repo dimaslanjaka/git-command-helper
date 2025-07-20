bc4135367b70aef15982787bddf5fa61119bc35c
dimaslanjaka <dimaslanjaka@gmail.com>
2025-07-20 22:03:36 +0700

    refactor(index.ts): streamline export syntax for git commands

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import git from "./git";
 export * from "./index-exports";
 export * as ext from "./index-exports";
+export { git as gitCommandHelper, git as gitHelper };
 
 export default git;
-export const gitHelper = git;
-export const gitCommandHelper = git;
