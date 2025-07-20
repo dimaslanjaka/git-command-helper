814bf7f6f8ca1e52ca95761dd42c20881b84a55e
dimaslanjaka <dimaslanjaka@gmail.com>
2025-07-20 20:42:12 +0700

    fix: replace console.error with noop in openHandles test to prevent unhandled promise rejections

--- test/openHandles.ts
+++ test/openHandles.ts
@@ -1,13 +1,14 @@
 import whyIsNodeRunning from "why-is-node-running";
 // should be imported before any other modules
 
+import { noop } from "sbg-utility";
 import src from "../src";
 import { repoDir, repoUrl } from "./env.cjs";
 
 (async () => {
   const gh = new src({ remote: repoUrl, cwd: repoDir });
   console.log("github root dir:", gh.cwd);
-  await gh.clone().catch(console.error);
+  await gh.clone().catch(noop);
 })();
 
 // logs out active handles that are keeping node running
