a86d076a8f84feaf87e4b559c1ca8326b8968b8c
dimaslanjaka <dimaslanjaka@gmail.com>
2025-07-24 08:08:15 +0700

    test(import): verify direct import of CJS bundle and class exports
    - Added inspection output to `import-direct.cjs` for debugging
    - Updated ESM test to dynamically import CJS bundle
    - Ensured class structure (`gitHelper`, `gitCommandHelper`) is preserved in dist output

--- test/import-direct.cjs
+++ test/import-direct.cjs
@@ -1,4 +1,5 @@
 const gch = require("../dist/index.js");
+const util = require("util");
 
 function isClass(obj) {
   return typeof obj === "function" && /^class\s/.test(Function.prototype.toString.call(obj));
@@ -9,4 +10,5 @@ if (require.main === module) {
   console.log(isClass(gch));
   console.log(isClass(gch.gitCommandHelper));
   console.log(isClass(gch.gitHelper));
+  console.log(util.inspect(gch, { depth: 1, colors: true }));
 }
