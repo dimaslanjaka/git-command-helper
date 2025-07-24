a96f37328be57a35f70a6d831cba5b53cabb7136
dimaslanjaka <dimaslanjaka@gmail.com>
2025-07-24 10:38:50 +0700

    chore: fix ESM export

--- test/import-direct.test.mjs
+++ test/import-direct.test.mjs
@@ -2,14 +2,18 @@ import { beforeAll, describe, expect, it } from "@jest/globals";
 import { isClass } from "./import-direct.cjs";
 
 describe("Import CJS and ESM dist bundles", () => {
+  /** @type {import("../dist/index.js")} */
   let gchCJS;
+  /** @type {import("../dist/index.mjs")} */
   let gchESM;
+
   beforeAll(async () => {
     // Import the CJS version to ensure it works correctly
     gchCJS = await import("../dist/index.js");
     // Import the ESM version to ensure it works correctly
     gchESM = await import("../dist/index.mjs");
   });
+
   it("CJS: gch is not a class", () => {
     expect(isClass(gchCJS)).toBe(false);
   });
@@ -24,6 +28,7 @@ describe("Import CJS and ESM dist bundles", () => {
 
   it("ESM: gch.gitCommandHelper is a class", () => {
     expect(isClass(gchESM.gitCommandHelper)).toBe(true);
+    console.log(typeof gchESM.gitCommandHelper); // Should log 'function' if it's a class
   });
 
   it("CJS: gch.gitHelper is a class", () => {
