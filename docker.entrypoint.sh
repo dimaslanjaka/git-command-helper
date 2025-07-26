196566ad1488345e94a7be46a68bbcce45741640
Dimas Lanjaka <dimaslanjaka@gmail.com>
2025-07-26 15:11:05 +0700

    refactor(docker): remove build step from entrypoint script, let `pretest.ts` handle it

--- docker.entrypoint.sh
+++ docker.entrypoint.sh
@@ -7,13 +7,6 @@ if command -v jq >/dev/null 2>&1; then
   jq 'del(.packageManager)' package.json > temp.json && mv temp.json package.json
 fi
 
-# Run build
-if [ -f package-lock.json ]; then
-  npm run build
-elif [ -f yarn.lock ]; then
-  yarn run build
-fi
-
 # Run tests
 if command -v tee >/dev/null 2>&1; then
   npm test 2>&1 | tee tmp/logs/node-14.test.log
