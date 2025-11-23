e7e52a6056f9d779418d60a94debb4cc6b1addf8
dimaslanjaka <dimaslanjaka@gmail.com>
2025-11-23 09:02:51 +0700

    feat: refactor submodule handling and add tests for extractSubmodule function

---
+++ test/utils/extract-submodule.direct.mjs
@@ -0,0 +1,8 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import extractSubmodule from "../../dist/utils/extract-submodule.mjs";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const gitmodulesPath = path.join(__dirname, '../fixtures/.gitmodules-sample');
+const submodules = extractSubmodule(gitmodulesPath);
+console.log(submodules);
